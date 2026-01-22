d0e8530a48670d68a650133ed17c2009a90a252a
oehab30 <omarehab785@gmail.com>
2026-01-22 14:43:36 +0200

    updated-project-pictures

--- src/components/projects/project_data.tsx
+++ src/components/projects/project_data.tsx
@@ -1,37 +1,31 @@
 export const projects = [
   {
     id: "01",
-    title: "Mazraaty Digital Agriculture Platform",
+    title: "Aurene",
     category: "React 18 • TypeScript • Tailwind CSS • shadcn ui • zod • tanstack query • axios •",
-    image: "../../../public/Screenshot 2026-01-22 142314.png",
+    image: "../../../public/aurene.png",
     link: "https://aurene.vercel.app/"
   },
   {
     id: "02",
-    title: "EFB System (Egyptian Food Bank)",
+    title: "Cavashop",
     category: "React 18 • TypeScript • Chakra UI",
-    image: "../../../public/aurene.png",
+    image: "../../../public/cava.png",
     link: "https://oehab30.github.io/cavashop/home"
   },
   {
     id: "03",
-    title: "Qamhawy (Agricultural Services)",
-    category: "React 18 • TypeScript • Tailwind CSS • shadcn ui",
-    image: "../../../public/cava.png",
-    link: "#"
-  },
-  {
-    id: "04",
-    title: "Takweed System (Plant Quarantine)",
+    title: "Cineplay",
     category: "React 18 • TypeScript • Tailwind CSS • shadcn ui",
     image: "../../../public/cineplay.png",
     link: "#"
   },
   {
-    id: "05",
-    title: "Egy Medya",
-    category: "React 18 • Tailwind CSS • TypeScript • shadcn ui",
+    id: "04",
+    title: "Nike Commerce",
+    category: "React 18 • TypeScript • Tailwind CSS • shadcn ui",
     image: "../../../public/nike-commerce.png",
     link: "#"
   }
+  
 ];
