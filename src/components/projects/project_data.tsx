6b650c059352382252340db97be083c69ff1200a
oehab30 <omarehab785@gmail.com>
2026-01-22 15:09:13 +0200

    fix-path-for -images

--- src/components/projects/project_data.tsx
+++ src/components/projects/project_data.tsx
@@ -3,28 +3,28 @@ export const projects = [
     id: "01",
     title: "Aurene",
     category: "React 18 • TypeScript • Tailwind CSS • shadcn ui • zod • tanstack query • axios •",
-    image: "../../../public/aurene.png",
+    image: "/public/aurene.png",
     link: "https://aurene.vercel.app/"
   },
   {
     id: "02",
     title: "Cavashop",
     category: "React 18 • TypeScript • Chakra UI",
-    image: "../../../public/cava.png",
+    image: "/public/cava.png",
     link: "https://oehab30.github.io/cavashop/home"
   },
   {
     id: "03",
     title: "Cineplay",
     category: "React 18 • TypeScript • Tailwind CSS • shadcn ui",
-    image: "../../../public/cineplay.png",
+    image: "/public/cineplay.png",
     link: "#"
   },
   {
     id: "04",
     title: "Nike Commerce",
     category: "React 18 • TypeScript • Tailwind CSS • shadcn ui",
-    image: "../../../public/nike-commerce.png",
+    image: "/public/nike-commerce.png",
     link: "#"
   }
   
