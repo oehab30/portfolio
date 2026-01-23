2795a7564b21ac73ef04efeeec712ea078a03d2f
oehab30 <omarehab785@gmail.com>
2026-01-23 09:13:08 +0200

    finalized-version

--- src/components/projects/project_data.tsx
+++ src/components/projects/project_data.tsx
@@ -3,28 +3,28 @@ export const projects = [
     id: "01",
     title: "Aurene",
     category: "React 18 • TypeScript • Tailwind CSS • shadcn ui • zod • tanstack query • axios •",
-    image: "/public/aurene.png",
+    image: "/public/pictures/aurene.jpg",
     link: "https://aurene.vercel.app/"
   },
   {
     id: "02",
     title: "Cavashop",
-    category: "React 18 • TypeScript • Chakra UI",
-    image: "/public/cava.png",
+    category: "HTML • CSS ",
+    image: "/public/pictures/cava.jpg",
     link: "https://oehab30.github.io/cavashop/home"
   },
   {
     id: "03",
     title: "Cineplay",
-    category: "React 18 • TypeScript • Tailwind CSS • shadcn ui",
-    image: "/public/cineplay.png",
+    category: "React 18 • TypeScript • Tailwind CSS • shadcn ui • axios •",
+    image: "/public/pictures/cineplay.jpg",
     link: "#"
   },
   {
     id: "04",
     title: "Nike Commerce",
-    category: "React 18 • TypeScript • Tailwind CSS • shadcn ui",
-    image: "/public/nike-commerce.png",
+    category: "React 18 • TypeScript • Tailwind CSS • shadcn ui • axios •",
+    image: "/public/pictures/Nike.jpg",
     link: "#"
   }
   
