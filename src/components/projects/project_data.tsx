14f35728a5d55f275bc4c0bff3a8d3516e7854d9
oehab30 <omarehab785@gmail.com>
2026-08-18 22:33:10 +0300

    added moonomena

--- src/components/projects/project_data.tsx
+++ src/components/projects/project_data.tsx
@@ -1,52 +1,59 @@
 export const projects = [
   {
     id: "01",
+    title: "Moonomena",
+    category: "**Next.js 16 • TypeScript • Tailwind CSS • shadcn/ui • Zod • Three.js • GSAP • Framer Motion • React Three Fiber•",
+    image: "/pictures/moonomena.png",
+    link: "https://moonomena.vercel.app/"
+  },
+  {
+    id: "02",
     title: "Aurene",
     category: "React 18 • TypeScript • Tailwind CSS • shadcn ui • zod • tanstack query • axios •",
     image: "/pictures/aurene.jpg",
     link: "https://aurene.vercel.app/"
   },
   {
-    id: "02",
+    id: "03",
     title: "Velora",
     category: "HTML • CSS • JS",
     image: "/pictures/velora.jpg",
     link: "https://oehab30.github.io/Velora/home.html"
   },
   {
-    id: "03",
+    id: "04",
     title: "Bakery",
     category: "React 19 • TypeScript • Tailwind CSS",
     image: "/pictures/SweetRise.jpg",
     link: "https://bakery-delta-liart.vercel.app/"
   },
   {
-    id: "04",
+    id: "05",
     title: "Productivity Hub",
     category: "React 19 • JavaScript • Tailwind CSS",
     image: "/pictures/Productivity Hub.png",
     link: "https://todolist-kappa-azure.vercel.app/"
   },
   {
-    id: "05",
+    id: "06",
     title: "Cavashop",
     category: "HTML • CSS ",
     image: "/pictures/cava.jpg",
     link: "https://oehab30.github.io/cavashop/home"
   },
   {
-    id: "06",
+    id: "07",
     title: "Cineplay",
     category: "React 18 • TypeScript • Tailwind CSS • shadcn ui • axios •",
     image: "/pictures/cineplay.jpg",
     link: "#"
   },
   {
-    id: "07",
+    id: "08",
     title: "Nike Commerce",
     category: "React 18 • TypeScript • Tailwind CSS • shadcn ui • axios •",
     image: "/pictures/Nike.jpg",
     link: "#"
   }
-  
+
 ];
